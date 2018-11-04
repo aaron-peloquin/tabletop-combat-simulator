b57d620c79cf31bf1fa526923bf2a70f55222693
Aaron <aaron4uk@gmail.com>
2018-11-04 13:07:06 -0600

    oops, fixed test

--- src/helpers/lookupCreatureHash.test.js
+++ src/helpers/lookupCreatureHash.test.js
@@ -3,17 +3,17 @@ import lookupCreatureHash from './lookupCreatureHash'
 const mockState = [
   {hash:"abc1d"},
   {hash:"de2fg"},
-  {hash:"ef8gh"}
+  {hash:"ef8gh"},
   {hash:"nk34j"}
 ]
 const fakeHash = "ef8gh"
 
 test('lookupCreatureHash() returns a number', () => {
-  const result = lookupCharacterHash(fakeHash, mockState)
+  const result = lookupCreatureHash(fakeHash, mockState)
   expect(typeof result).toBe("number")
 })
 
 test('lookupCreatureHash() finds a match', () => {
-  const result = lookupCharacterHash(fakeHash, mockState)
+  const result = lookupCreatureHash(fakeHash, mockState)
   expect(result).toBe(2)
 })
