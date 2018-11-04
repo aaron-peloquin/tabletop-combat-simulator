cfd0090667e1f28fa2f97ab5f290f73b2c715dab
Aaron <aaron4uk@gmail.com>
2018-11-04 13:07:34 -0600

    added comment and first action

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,6 +1,10 @@
-
+/**
+ * This is an object of Redux Actions. It mostly exists to
+ *  ensure we do not create duplicate actions, but it can
+ *  also be used as a list of our existing actions
+*/
 const actions = {
-  actionDescription: "uniqueActionKey",
+  CreateCreature: "CreateCreature",
 }
 
 export default actions
