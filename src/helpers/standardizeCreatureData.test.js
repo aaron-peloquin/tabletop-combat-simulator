e777ce4c787257456f6c159a4c38616718bddb30
Aaron <aaron4uk@gmail.com>
2018-11-05 15:35:03 -0600

    adding tests

--- src/helpers/standardizeCreatureData.test.js
+++ src/helpers/standardizeCreatureData.test.js
@@ -1,13 +1,24 @@
 import standardizeCreatureData from "./standardizeCreatureData"
 
+const mockState = [{"name":"some mock data","hash":"fakeHash"}]
+
 test("standardizeCreatureData() returns an object with specific keys", () => {
-  const result = standardizeCreatureData({},[])
-
+  const result = standardizeCreatureData({}, mockState)
   expect(typeof result).toBe("object")
-
   expect(Object.keys(result)).toEqual([
     "name","hash","description",
     "defaultInitiative","armor","hp",
     "hitDiceEquation","damageDiceEquation"
   ])
+})
+
+test("standardizeCreatureData() check important default values", () => {
+  const result = standardizeCreatureData({}, mockState)
+  expect(typeof result).toBe("object")
+  expect(typeof result.name).toBe("string")
+  expect(result.name.length).toBeGreaterThan(0)
+  expect(result.hash.length).toBe(8)
+  expect(result.defaultInitiative).toBe(10)
+  expect(result.armor).toBe(10)
+  expect(result.hp).toBe(15)
 })
