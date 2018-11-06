d7c1da959ac886e19a17a949177c09c3847f00f1
Aaron <aaron4uk@gmail.com>
2018-11-06 05:02:22 -0600

    testing remove json call on pageload

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,15 @@
+import "isomorphic-fetch"
+
 const about = () => {
   return <p>About page content</p>
 }
 
 about.getInitialProps = async ({ req }) => {
-  return { title: "About" }
+  let title = "About"
+  let data = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()).then(data => {
+    title = data.title
+  })
+  return { title }
 }
 
 export default about
