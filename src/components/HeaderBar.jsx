46afb9961f794a7fe7747c93901870331462cf98
Aaron <aaron4uk@gmail.com>
2018-11-07 05:49:09 -0600

    started connecting to new reducer, but had to leave - not complete yet

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -3,8 +3,11 @@ import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
 import IconButton from "@material-ui/core/IconButton"
 import MenuIcon from "@material-ui/icons/Menu"
+import { connect } from 'react-redux'
 
-export default () => <AppBar>
+import ToggleSidebar from "./../store/dispatcher/ToggleSidebar"
+
+const HeaderBar = () => <AppBar>
   <Toolbar>
     <IconButton color="inherit" aria-label="Menu">
       <MenuIcon />
@@ -14,3 +17,5 @@ export default () => <AppBar>
     </Typography>
   </Toolbar>
 </AppBar>
+
+export default connect()(HeaderBar)
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,1 +1,10 @@
-export default () => <p>Side Bar</p>
+import { connect } from "react-redux"
+
+import ToggleSidebar from "./../store/dispatcher/ToggleSidebar"
+
+const SideBar = (props) => {
+  const { dispatch } = props
+
+}
+
+export default connect()(SideBar)
