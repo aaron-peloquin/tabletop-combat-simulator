2426b4f1109e7ac9e412beb9b8bd8493af0d11be
Aaron <aaron4uk@gmail.com>
2018-11-07 18:59:50 -0600

    added sidebar toggle to redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,18 @@
-import { createStore, applyMiddleware, combineReducers } from "redux"
-import { composeWithDevTools } from "redux-devtools-extension"
-import thunkMiddleware from "redux-thunk"
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from "redux-thunk";
 
 /** Reducers */
-import creatures from "./reducer/creatures"
+import creatures from "./reducer/creatures";
+import sideBar from "./reducer/sideBar";
 
 /** Reducers */
 const reducers = combineReducers({
-  creatures
-})
+  creatures,
+  sideBar,
+});
 
 /** initialize store */
 export function initializeStore (initialState = initialState) {
-  return createStore(reducers, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)))
+  return createStore(reducers, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)));
 }
