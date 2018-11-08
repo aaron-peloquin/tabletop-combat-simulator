3a8d49f1befed74d34df595b9169915a63b470eb
Aaron <aaron4uk@gmail.com>
2018-11-07 19:01:19 -0600

    connecting to redux and started building out material ui components

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -1,21 +1,28 @@
-import AppBar from "@material-ui/core/AppBar"
-import Toolbar from "@material-ui/core/Toolbar"
-import Typography from "@material-ui/core/Typography"
-import IconButton from "@material-ui/core/IconButton"
-import MenuIcon from "@material-ui/icons/Menu"
-import { connect } from 'react-redux'
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { connect } from "react-redux";
 
-import ToggleSidebar from "./../store/dispatcher/ToggleSidebar"
+import ToggleSidebar from "./../store/dispatcher/ToggleSidebar";
 
-const HeaderBar = () => <AppBar>
-  <Toolbar>
-    <IconButton color="inherit" aria-label="Menu">
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" color="inherit">
-      Tabletop Combat Simulator
-    </Typography>
-  </Toolbar>
-</AppBar>
+const HeaderBar = (props) => {
+  const {dispatch} = props;
+  return <AppBar>
+    <Toolbar>
+      <IconButton color="inherit" aria-label="Menu" onClick={()=>{ return ToggleSidebar(dispatch); }} >
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" color="inherit">
+        Tabletop Combat Simulator
+      </Typography>
+    </Toolbar>
+  </AppBar>
+};
 
-export default connect()(HeaderBar)
+const mapStateToProps = (state) => {
+  return {open:state.sideBar};
+}
+
+export default connect(mapStateToProps)(HeaderBar);
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,28 @@
-import { connect } from "react-redux"
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  IconButton,
+} from "@material-ui/core/";
+import Link from "next/link";
 
-import ToggleSidebar from "./../store/dispatcher/ToggleSidebar"
+import { connect } from "react-redux";
+
+import ToggleSidebar from "./../store/dispatcher/ToggleSidebar";
 
 const SideBar = (props) => {
-  const { dispatch } = props
-
+  const { dispatch, open } = props
+  return <Drawer open={open} anchor="left" onOpen={()=>{ return ToggleSidebar(dispatch); }} onClose={()=>{ return ToggleSidebar(dispatch); }}>
+    <Link href="/about" onClick={()=>{ return ToggleSidebar(dispatch); }}><a>About</a></Link>
+  </Drawer>;
 }
 
-export default connect()(SideBar)
+const mapStateToProps = (state) => {
+  console.log("ALERT",state);
+  return {open:state.sideBar};
+}
+
+export default connect(mapStateToProps)(SideBar);
