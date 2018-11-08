2125719f1c73d70d8718aecaaa441d412aef92c6
Aaron <aaron4uk@gmail.com>
2018-11-07 19:00:42 -0600

    _app setup and moved header and sidebar into pages

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,25 +1,21 @@
-import { Container } from "next/app"
-import React from "react"
-import { Provider } from "react-redux"
-import Head from "next/head"
+import { Container } from "next/app";
+import React from "react";
+import { Provider } from "react-redux";
+import Head from "next/head";
 
-import withReduxStore from "../store/AppWithRedux"
-import SideBar from "../components/SideBar"
-import HeaderBar from "./../components/HeaderBar"
+import withReduxStore from "../store/AppWithRedux";
 
-const defaultWebsiteTitle = "Default Website Title"
+const defaultWebsiteTitle = "Default Website Title";
 
 const MyApp = (props) => {
-  const {Component, pageProps, router, reduxStore} = props
-  const {title=defaultWebsiteTitle} = pageProps
+  const {Component, pageProps, router, reduxStore} = props;
+  const {title=defaultWebsiteTitle} = pageProps;
 
   return (
     <Container>
       <Head>
         <title>{title}</title>
       </Head>
-      <HeaderBar />
-      <SideBar />
       <Provider store={reduxStore}>
         <Component {...pageProps} router={router} />
       </Provider>
@@ -28,19 +24,19 @@ const MyApp = (props) => {
 }
 
 MyApp.getInitialProps = async ({ Component, ctx }) => {
-  let pageProps = {}
+  let pageProps = {};
   /** If this page has getInitialProps(), run it to fill pageProps */
   if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx)
+    pageProps = await Component.getInitialProps(ctx);
   }
   /** Extract pageProp title, or set title to the default */
-  const {title = defaultWebsiteTitle} = pageProps
+  const {title = defaultWebsiteTitle} = pageProps;
   /** If we have a document, update it'"s title for UX */
   if (typeof document != "undefined" ) {
-    document.title = title
+    document.title = title;
   }
 
-  return { pageProps }
+  return { pageProps };
 }
 
-export default withReduxStore(MyApp)
+export default withReduxStore(MyApp);
