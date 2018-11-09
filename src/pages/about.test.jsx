ba1e6910d8b1e928b393e8f8661b8e4ead814071
Aaron <aaron4uk@gmail.com>
2018-11-09 05:44:04 -0600

    snapshots working

--- src/pages/about.test.jsx
+++ src/pages/about.test.jsx
@@ -1,8 +1,9 @@
 // import renderer from "react-test-renderer";
 import { connect } from "react-redux";
-import { mountWithStore } from "enzyme-redux";
+import { shallowWithStore } from "enzyme-redux";
 import { createMockStore } from "redux-test-utils";
 import mockStoreState from "./../testHelpers/mockStoreState";
+import toJson from "enzyme-to-json";
 
 import About from "./about";
 
@@ -12,12 +13,12 @@ describe("<About />", ()=>{
     expect(props.title).toBe("About");
   });
 
-  // it("Snapshots", () => {
-  //   const ReactComponent = () => (<About />);
-  //   const mapStateToProps = (state) => ({state});
-  //   const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
-  //   const aboutPage = mountWithStore(ConnectedComponent, createMockStore(mockStoreState));
-  //   const tree = aboutPage.toJSON();
-  //   expect(tree).toMatchSnapshot();
-  // });
+  it("Snapshots", () => {
+    const ReactComponent = () => (<About />);
+    const mapStateToProps = (state) => ({state});
+    const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
+    const aboutPage = shallowWithStore(<ConnectedComponent />, createMockStore(mockStoreState));
+    const tree = aboutPage.dive();
+    expect(toJson(tree)).toMatchSnapshot();
+  });
 });
