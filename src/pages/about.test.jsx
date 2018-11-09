bf2accbcb9e0239000916f4d654da46227b3e2fb
Aaron <aaron4uk@gmail.com>
2018-11-09 16:01:03 -0600

    added unit test for basic loading

--- src/pages/about.test.jsx
+++ src/pages/about.test.jsx
@@ -8,13 +8,18 @@ import toJson from "enzyme-to-json";
 import About from "./about";
 
 describe("<About />", ()=>{
-  it("has a title of About", async () => {
+  it("loads", async () => {
+    const ReactComponent = () => <About />;
+    expect(typeof ReactComponent).toBe("function");
+  });
+
+  it("has a NextJS prop.title of About", async () => {
     const props = await About.getInitialProps();
     expect(props.title).toBe("About");
   });
 
   it("Snapshots", () => {
-    const ReactComponent = () => (<About />);
+    const ReactComponent = () => <About />;
     const mapStateToProps = (state) => ({state});
     const store = createMockStore(mockStoreState);
     const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
