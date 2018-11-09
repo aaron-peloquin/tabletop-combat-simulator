3279329fc975d27d3d329f5a31ae329836f0998f
Aaron <aaron4uk@gmail.com>
2018-11-09 15:55:24 -0600

    rewrote to use deep rendering of shadow dom on snapshot

--- src/pages/about.test.jsx
+++ src/pages/about.test.jsx
@@ -1,6 +1,6 @@
 // import renderer from "react-test-renderer";
 import { connect } from "react-redux";
-import { shallowWithStore } from "enzyme-redux";
+import { mountWithStore } from "enzyme-redux";
 import { createMockStore } from "redux-test-utils";
 import mockStoreState from "./../testHelpers/mockStoreState";
 import toJson from "enzyme-to-json";
@@ -16,9 +16,9 @@ describe("<About />", ()=>{
   it("Snapshots", () => {
     const ReactComponent = () => (<About />);
     const mapStateToProps = (state) => ({state});
+    const store = createMockStore(mockStoreState);
     const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
-    const aboutPage = shallowWithStore(<ConnectedComponent />, createMockStore(mockStoreState));
-    const tree = aboutPage.dive();
-    expect(toJson(tree)).toMatchSnapshot();
+    const component = mountWithStore(<ConnectedComponent />, store);
+    expect(toJson(component)).toMatchSnapshot();
   });
 });
