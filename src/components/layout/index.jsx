c71f00f7280dbee0383ffbf3fb9449d0e70cff83
Aaron <aaron4uk@gmail.com>
2018-11-10 15:33:48 -0600

    added basic layout styles

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,12 +1,38 @@
+import {Grid,Paper} from "@material-ui/core";
+import {withStyles} from "@material-ui/core/styles";
+
 import HeaderBar from "./HeaderBar";
 import SideBar from "./SideBar";
 
-const Layout = ({children="No page content was passed"}) => (
+const styles = (theme) => {
+  return {
+    content: {
+      margin: "0 auto",
+      marginTop: "90px",
+      //maxWidth: "1000px",
+      width: "80%",
+    },
+    paper: {
+      minHeight: "500px",
+      padding: theme.spacing.unit * 4,
+    }
+  };
+};
+
+const Layout = ({children="No page content was passed",classes}) => (
   <div>
     <HeaderBar />
     <SideBar />
-    {children}
+    <main className={classes.content}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            {children}
+          </Paper>
+        </Grid>
+      </Grid>
+    </main>
   </div>
 );
 
-export default Layout;
+export default withStyles(styles)(Layout);
