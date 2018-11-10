a656fc62dc91ebec8ae55e451dc7dc06d1cba011
Aaron <aaron4uk@gmail.com>
2018-11-09 20:30:53 -0600

    added icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,27 +4,38 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Typography,
-  IconButton,
 } from "@material-ui/core/";
+import {
+  Home,
+  Info,
+  ViewList,
+} from "@material-ui/icons";
 import Link from "next/link";
 import { connect } from "react-redux";
 
 import ToggleSidebar from "./../store/dispatcher/ToggleSidebar";
 
 const NavigationLink = (props) => {
-  const {dispatch, url, children} = props;
-  return (<Link href={url}>
-    <a onClick={()=>{ return ToggleSidebar(dispatch); }}>{children}</a>
-  </Link>);
-}
+  const {dispatch, text, url, children} = props;
+  return <ListItem>
+    <Link href={url}>
+      <a onClick={()=>{ return ToggleSidebar(dispatch); }}>
+        {children?<ListItemIcon>{children}</ListItemIcon>:""}
+        <ListItemText primary={text} />
+      </a>
+    </Link>
+  </ListItem>;
+};
 
 const SideBar = (props) => {
   const { dispatch, open } = props;
   return <Drawer open={open} anchor="left" onClose={()=>{ return ToggleSidebar(dispatch); }}>
-    <NavigationLink dispatch={dispatch} url="/">Home</NavigationLink>
-    <NavigationLink dispatch={dispatch} url="/about">About</NavigationLink>
-    <NavigationLink dispatch={dispatch} url="/creatures">Creatures</NavigationLink>
+    <List>
+      <NavigationLink dispatch={dispatch} text="Home" url="/"><Home /></NavigationLink>
+      <NavigationLink dispatch={dispatch} text="About" url="/about"><Info /></NavigationLink>
+      <NavigationLink dispatch={dispatch} text="Creatures" url="/creatures"><ViewList /></NavigationLink>
+      <NavigationLink dispatch={dispatch} text="Combat Simulator" url="/combat-simulator"></NavigationLink>
+    </List>
   </Drawer>;
 };
 
