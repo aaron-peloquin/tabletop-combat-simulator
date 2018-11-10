f213bfe1f7c53e65c6be4bd5ce95fa584d678c89
Aaron <aaron4uk@gmail.com>
2018-11-10 09:31:27 -0600

    replace redux implementation with a more straightforward and more powerful solution

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,40 +3,46 @@ import React from "react";
 import { Provider } from "react-redux";
 import Head from "next/head";
 
-import withReduxStore from "../store/AppWithRedux";
+import {initializeStore} from "../store/store";
+import withRedux from "next-redux-wrapper";
+import Layout from "./../components/layout";
 
 const defaultWebsiteTitle = "Default Website Title";
 
 const MyApp = (props) => {
-  const {Component, pageProps, router, reduxStore} = props;
+  const {Component, pageProps, store} = props;
   const {title=defaultWebsiteTitle} = pageProps;
-
   return (
     <Container>
       <Head>
         <title>{title}</title>
       </Head>
-      <Provider store={reduxStore}>
-        <Component {...pageProps} router={router} />
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </Provider>
     </Container>
-  )
-}
+
+  );};
 
 MyApp.getInitialProps = async ({ Component, ctx }) => {
   let pageProps = {};
+
   /** If this page has getInitialProps(), run it to fill pageProps */
   if (Component.getInitialProps) {
     pageProps = await Component.getInitialProps(ctx);
   }
+
   /** Extract pageProp title, or set title to the default */
   const {title = defaultWebsiteTitle} = pageProps;
+
   /** If we have a document, update it'"s title for UX */
   if (typeof document != "undefined" ) {
     document.title = title;
   }
 
   return { pageProps };
-}
+};
 
-export default withReduxStore(MyApp);
+export default withRedux(initializeStore,{debug: true})(MyApp);
