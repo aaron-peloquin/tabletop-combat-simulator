dcaf24d24fe6bc8b296f748c445ab88988b7bd34
Aaron <aaron4uk@gmail.com>
2018-11-10 13:02:09 -0600

    cleaned up code, added comments

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,30 +1,27 @@
-import {
-  SwipeableDrawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-} from "@material-ui/core/";
-import {
-  Home,
-  Info,
-  ViewList,
-} from "@material-ui/icons";
-import Link from "next/link";
-import { connect } from "react-redux";
+import { SwipeableDrawer, List, ListItem, ListItemIcon, ListItemText, } from "@material-ui/core/";
 import { withStyles } from "@material-ui/core/styles";
+import { Home, Info, ViewList, } from "@material-ui/icons";
+import { connect } from "react-redux";
+import Link from "next/link";
 
 import ToggleSidebar from "./../../store/dispatcher/ToggleSidebar";
 
+/** MUI Styles for <SideBar />, and it's internal <SideBarLink /> */
 const styles = {
-  drawerContainer: {
-    width: 250,
-  },
-  link: {
-    textDecoration: "none",
-  },
+  drawerContainer: { width: 250, },
+  link: { textDecoration: "none", },
 };
-const NavigationLink = (props) => {
+
+/**
+ * Component for links as list items
+ * @param {obj} props
+    * @param {str} classes MUI object for CSS
+    * @param {str} text The text of this link
+    * @param {str} url The href
+    * @param {jsx} children Optional icon
+    * @return {jsx} <Link /> component to be placed inside of a <ListItem />
+ */
+const SideBarLink = (props) => {
   const {text, url, children, classes} = props;
   return <Link href={url}>
     <a className={classes.link}>
@@ -36,6 +33,9 @@ const NavigationLink = (props) => {
   </Link>;
 };
 
+/**
+ * Swipable drawer for navigation
+ */
 const SideBar = (props) => {
   const { dispatch, open, classes } = props;
   return <SwipeableDrawer onOpen={()=>{ToggleSidebar(dispatch);}} open={open} anchor="left" onClose={()=>{ToggleSidebar(dispatch);}}>
@@ -46,18 +46,21 @@ const SideBar = (props) => {
       onKeyDown={()=>{ToggleSidebar(dispatch);}}
     >
       <List>
-        <NavigationLink classes={classes} text="Home" url="/"><Home /></NavigationLink>
-        <NavigationLink classes={classes} text="About" url="/about"><Info /></NavigationLink>
-        <NavigationLink classes={classes} text="Creatures" url="/creatures"><ViewList /></NavigationLink>
-        <NavigationLink classes={classes} text="Combat Simulator" url="/combat-simulator"></NavigationLink>
+        <SideBarLink classes={classes} text="Home" url="/"><Home /></SideBarLink>
+        <SideBarLink classes={classes} text="About" url="/about"><Info /></SideBarLink>
+        <SideBarLink classes={classes} text="Creatures" url="/creatures"><ViewList /></SideBarLink>
+        <SideBarLink classes={classes} text="Combat Simulator" url="/combat-simulator"></SideBarLink>
       </List>
     </div>
   </SwipeableDrawer>;
 };
 
+/** Map the {redux}.SideBar to the <SideBar /> component */
 const mapStateToProps = (state) => {
   return {open:state.sideBar};
 };
 
+/** Add MUI styles to this component */
 const SideBarWithStyles = withStyles(styles)(SideBar);
+/** Connect this component to Redux */
 export default connect(mapStateToProps)(SideBarWithStyles);
