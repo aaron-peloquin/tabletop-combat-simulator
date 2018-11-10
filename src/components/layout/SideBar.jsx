d77a4cd6a2e08a4c27398641845340d22c06a322
Aaron <aaron4uk@gmail.com>
2018-11-10 12:49:31 -0600

    navigation working and styled

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -17,36 +17,39 @@ import { withStyles } from "@material-ui/core/styles";
 import ToggleSidebar from "./../../store/dispatcher/ToggleSidebar";
 
 const styles = {
-  fullWidth: {
+  drawerContainer: {
     width: 250,
+  },
+  link: {
+    textDecoration: "none",
   },
 };
 const NavigationLink = (props) => {
-  const {text, url, children} = props;
-  return <ListItem button>
-    <Link href={url}>
-      <a>
+  const {text, url, children, classes} = props;
+  return <Link href={url}>
+    <a className={classes.link}>
+      <ListItem button>
         {children?<ListItemIcon>{children}</ListItemIcon>:""}
         <ListItemText primary={text} />
-      </a>
-    </Link>
-  </ListItem>;
+      </ListItem>
+    </a>
+  </Link>;
 };
 
 const SideBar = (props) => {
   const { dispatch, open, classes } = props;
   return <SwipeableDrawer onOpen={()=>{ToggleSidebar(dispatch);}} open={open} anchor="left" onClose={()=>{ToggleSidebar(dispatch);}}>
     <div
-      className={classes.fullWidth}
+      className={classes.drawerContainer}
       role="button"
       onClick={()=>{ToggleSidebar(dispatch);}}
       onKeyDown={()=>{ToggleSidebar(dispatch);}}
     >
       <List>
-        <NavigationLink dispatch={dispatch} text="Home" url="/"><Home /></NavigationLink>
-        <NavigationLink dispatch={dispatch} text="About" url="/about"><Info /></NavigationLink>
-        <NavigationLink dispatch={dispatch} text="Creatures" url="/creatures"><ViewList /></NavigationLink>
-        <NavigationLink dispatch={dispatch} text="Combat Simulator" url="/combat-simulator"></NavigationLink>
+        <NavigationLink classes={classes} text="Home" url="/"><Home /></NavigationLink>
+        <NavigationLink classes={classes} text="About" url="/about"><Info /></NavigationLink>
+        <NavigationLink classes={classes} text="Creatures" url="/creatures"><ViewList /></NavigationLink>
+        <NavigationLink classes={classes} text="Combat Simulator" url="/combat-simulator"></NavigationLink>
       </List>
     </div>
   </SwipeableDrawer>;
