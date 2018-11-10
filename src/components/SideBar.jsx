e8757f9ba3646783f495371bb482043678a3f669
Aaron <aaron4uk@gmail.com>
2018-11-09 18:23:03 -0600

    broke <Link /> out into it's own internally reusable component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,15 +8,23 @@ import {
   IconButton,
 } from "@material-ui/core/";
 import Link from "next/link";
-
 import { connect } from "react-redux";
 
 import ToggleSidebar from "./../store/dispatcher/ToggleSidebar";
 
+const NavigationLink = (props) => {
+  const {dispatch, url, children} = props;
+  return (<Link href={url}>
+    <a onClick={()=>{ return ToggleSidebar(dispatch); }}>{children}</a>
+  </Link>);
+}
+
 const SideBar = (props) => {
   const { dispatch, open } = props;
-  return <Drawer open={open} anchor="left" onOpen={()=>{ return ToggleSidebar(dispatch); }} onClose={()=>{ return ToggleSidebar(dispatch); }}>
-    <Link href="/about"><a onClick={()=>{ return ToggleSidebar(dispatch); }}>About</a></Link>
+  return <Drawer open={open} anchor="left" onClose={()=>{ return ToggleSidebar(dispatch); }}>
+    <NavigationLink dispatch={dispatch} url="/">Home</NavigationLink>
+    <NavigationLink dispatch={dispatch} url="/about">About</NavigationLink>
+    <NavigationLink dispatch={dispatch} url="/creatures">Creatures</NavigationLink>
   </Drawer>;
 };
 
