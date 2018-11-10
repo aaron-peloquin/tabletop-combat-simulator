9369578a15ed5e6a13761b99e6de61817a2d3fda
Aaron <aaron4uk@gmail.com>
2018-11-10 12:17:02 -0600

    removing layout elements

--- src/pages/creatures.jsx
+++ src/pages/creatures.jsx
@@ -1,11 +1,7 @@
-import SideBar from "../components/SideBar";
-import HeaderBar from "./../components/HeaderBar";
 import ListCreatures from "./../components/ListCreatures";
 
 const creatures = () => {
   return <p>
-    <HeaderBar />
-    <SideBar />
     <ListCreatures />
   </p>;
 };
