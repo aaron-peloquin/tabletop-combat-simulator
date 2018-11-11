29d96e78051141701c176237dfc1f5ef267ab562
Aaron <aaron4uk@gmail.com>
2018-11-10 19:30:53 -0600

    broke server out into two files

--- src/server.js
+++ src/server.js
@@ -1,26 +1,29 @@
-const express = require("express")
-const next = require("next")
+const express = require("express");
+const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== "production"
-const dir = "./src/"
-const app = next({ dev, dir })
-const handle = app.getRequestHandler()
-
-app.prepare()
-  .then(() => {
-    const server = express()
+const server = (dev) => {
+  const port = (dev?3000:80);
+  const dir = "./src/";
+  const app = next({ dev, dir });
+  const handle = app.getRequestHandler();
+  app.prepare().then(() => {
+    const server = express();
 
     server.get("/character/:hash", (req, res) => {
-      return app.render(req, res, "/character", { hash: req.params.hash })
-    })
+      return app.render(req, res, "/character", { hash: req.params.hash });
+    });
 
     server.get("*", (req, res) => {
-      return handle(req, res)
-    })
+      return handle(req, res);
+    });
 
     server.listen(port, (err) => {
-      if (err) throw err
-      console.log(`> Ready on http://localhost:${port}`)
-    })
-  })
+      if (err) {
+        throw err;
+      }
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  });
+};
+
+module.exports = server;
