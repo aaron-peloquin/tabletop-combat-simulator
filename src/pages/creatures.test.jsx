8fc9a19e5f18248d76a7710d4719e0211d9da32e
Aaron <aaron4uk@gmail.com>
2018-11-12 16:18:40 -0600

    initial commit

---
+++ src/pages/creatures.test.jsx
@@ -0,0 +1,24 @@
+// import renderer from "react-test-renderer"
+import { connect } from "react-redux"
+import { mountWithStore } from "enzyme-redux"
+import { createMockStore } from "redux-test-utils"
+import mockStoreState from "./../testHelpers/mockStoreState"
+import toJson from "enzyme-to-json"
+
+import Creatures from "./creatures"
+
+describe("<About />", ()=>{
+  it("loads", async () => {
+    const ReactComponent = () => <Creatures />
+    expect(typeof ReactComponent).toBe("function")
+  })
+
+  it("Snapshots", () => {
+    const ReactComponent = () => <Creatures />
+    const mapStateToProps = (state) => ({state})
+    const store = createMockStore(mockStoreState)
+    const ConnectedComponent = connect(mapStateToProps)(ReactComponent)
+    const component = mountWithStore(<ConnectedComponent />, store)
+    expect(toJson(component)).toMatchSnapshot()
+  })
+})
---
+++ src/pages/index.test.jsx
@@ -0,0 +1,24 @@
+// import renderer from "react-test-renderer"
+import { connect } from "react-redux"
+import { mountWithStore } from "enzyme-redux"
+import { createMockStore } from "redux-test-utils"
+import mockStoreState from "./../testHelpers/mockStoreState"
+import toJson from "enzyme-to-json"
+
+import Index from "./index"
+
+describe("<About />", ()=>{
+  it("loads", async () => {
+    const ReactComponent = () => <Index />
+    expect(typeof ReactComponent).toBe("function")
+  })
+
+  it("Snapshots", () => {
+    const ReactComponent = () => <Index />
+    const mapStateToProps = (state) => ({state})
+    const store = createMockStore(mockStoreState)
+    const ConnectedComponent = connect(mapStateToProps)(ReactComponent)
+    const component = mountWithStore(<ConnectedComponent />, store)
+    expect(toJson(component)).toMatchSnapshot()
+  })
+})
