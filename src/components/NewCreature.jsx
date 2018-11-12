1b9a0b243357b664c548e4707d0a5fd7f1a85519
Aaron <aaron4uk@gmail.com>
2018-11-12 17:34:45 -0600

    semi cleanse

--- src/components/NewCreature.jsx
+++ src/components/NewCreature.jsx
@@ -1,18 +1,18 @@
-import {connect} from "react-redux";
-import {Button} from "@material-ui/core/";
+import {connect} from "react-redux"
+import {Button} from "@material-ui/core/"
 
-import CreateCreature from "./../store/dispatcher/CreateCreature";
+import CreateCreature from "./../store/dispatcher/CreateCreature"
 
 const makeNewCreature = (dispatch) => {
-  CreateCreature(dispatch);
+  CreateCreature(dispatch)
   
 };
 
 const NewCreature = (props) => {
-  const {dispatch} = props;
-  return <Button variant="contained" color="primary" onClick={()=>{makeNewCreature(dispatch);}}>
+  const {dispatch} = props
+  return <Button variant="contained" color="primary" onClick={()=>{makeNewCreature(dispatch)}}>
     New Creature
-  </Button>;
+  </Button>
 };
 
-export default connect()(NewCreature);
+export default connect()(NewCreature)
