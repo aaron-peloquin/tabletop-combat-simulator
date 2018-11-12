9c1728d1a4181625d9ca0d2a34e6700b3a41f215
Aaron <aaron4uk@gmail.com>
2018-11-12 16:20:35 -0600

    semi cleanse

--- src/pages/about.test.jsx
+++ src/pages/about.test.jsx
@@ -1,29 +1,28 @@
-// import renderer from "react-test-renderer";
-import { connect } from "react-redux";
-import { mountWithStore } from "enzyme-redux";
-import { createMockStore } from "redux-test-utils";
-import mockStoreState from "./../testHelpers/mockStoreState";
-import toJson from "enzyme-to-json";
+import { connect } from "react-redux"
+import { mountWithStore } from "enzyme-redux"
+import { createMockStore } from "redux-test-utils"
+import mockStoreState from "./../testHelpers/mockStoreState"
+import toJson from "enzyme-to-json"
 
-import About from "./about";
+import About from "./about"
 
 describe("<About />", ()=>{
   it("loads", async () => {
-    const ReactComponent = () => <About />;
-    expect(typeof ReactComponent).toBe("function");
-  });
+    const ReactComponent = () => <About />
+    expect(typeof ReactComponent).toBe("function")
+  })
 
   it("has a NextJS prop.title of About", async () => {
-    const props = await About.getInitialProps();
-    expect(props.title).toBe("About");
-  });
+    const props = await About.getInitialProps()
+    expect(props.title).toBe("About")
+  })
 
   it("Snapshots", () => {
-    const ReactComponent = () => <About />;
-    const mapStateToProps = (state) => ({state});
-    const store = createMockStore(mockStoreState);
-    const ConnectedComponent = connect(mapStateToProps)(ReactComponent);
-    const component = mountWithStore(<ConnectedComponent />, store);
-    expect(toJson(component)).toMatchSnapshot();
-  });
-});
+    const ReactComponent = () => <About />
+    const mapStateToProps = (state) => ({state})
+    const store = createMockStore(mockStoreState)
+    const ConnectedComponent = connect(mapStateToProps)(ReactComponent)
+    const component = mountWithStore(<ConnectedComponent />, store)
+    expect(toJson(component)).toMatchSnapshot()
+  })
+})
