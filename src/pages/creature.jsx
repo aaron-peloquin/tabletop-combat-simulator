2bf5f60ee826bfbd86a0229d23439539a56d74b4
Aaron <aaron4uk@gmail.com>
2018-11-12 05:48:21 -0600

    started building out component

--- src/pages/creature.jsx
+++ src/pages/creature.jsx
@@ -1,5 +1,31 @@
-const creature = () => {
-  return <p>Single creature page</p>
+import React, { Fragment } from "react"
+import { connect } from "react-redux"
+
+import lookupCreatureHash from "./../helpers/lookupCreatureHash"
+
+let creature = props => {
+  const { id, char } = props
+
+  return <Fragment>
+    <h1>Creature: {id}</h1>
+  </Fragment>
 }
 
-export default creature
+creature.getInitialProps = async () => {
+  let title = "Creature"
+  return { title }
+}
+
+const mapStateToProps = (state, props) => {
+  const {hash=0} = props.router.query
+  const id = lookupCreatureHash(hash,state.creatures)
+  let creature
+  if(id>-1) {
+    creature = state.creatures[id]||{}
+  }
+  
+  return {creature}
+}
+
+const creatureWithState = connect(mapStateToProps)(creature)
+export default creatureWithState
