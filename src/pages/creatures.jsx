f0ce38d00fb5f653ed6491d6b0e1a35669ad7ccb
Aaron <aaron4uk@gmail.com>
2018-11-12 05:37:58 -0600

    removing semi

--- src/pages/creatures.jsx
+++ src/pages/creatures.jsx
@@ -1,17 +1,17 @@
-import {Fragment} from "react";
-import NewCreature from "../components/NewCreature";
-import ListCreatures from "./../components/ListCreatures";
+import {Fragment} from "react"
+import NewCreature from "../components/NewCreature"
+import ListCreatures from "./../components/ListCreatures"
 
 const creatures = () => {
   return <Fragment>
     <NewCreature />
     <ListCreatures />
-  </Fragment>;
-};
+  </Fragment>
+}
 
 creatures.getInitialProps = async () => {
-  let title = "Creatures Database";
-  return { title };
-};
+  let title = "Creatures Database"
+  return { title }
+}
 
-export default creatures;
+export default creatures
