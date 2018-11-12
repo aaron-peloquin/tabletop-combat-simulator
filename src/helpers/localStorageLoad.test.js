25daaa53d90613e8ebeda992e2a72818c41f3b55
Aaron <aaron4uk@gmail.com>
2018-11-12 16:18:02 -0600

    semi cleanse

--- src/helpers/localStorageLoad.test.js
+++ src/helpers/localStorageLoad.test.js
@@ -1,10 +1,10 @@
-import localStorageLoad from "./localStorageLoad";
+import localStorageLoad from "./localStorageLoad"
 
 describe("[helper] localStorageLoad()", ()=>{
   it("can load", async () => {
-    localStorage.setItem("ttcs-testLoad",JSON.stringify({test:true}));
-    const test = localStorageLoad("testLoad");
-    localStorage.removeItem("ttcs-testLoad");
-    expect(test).toEqual({test:true});
-  });
-});
+    localStorage.setItem("ttcs-testLoad",JSON.stringify({test:true}))
+    const test = localStorageLoad("testLoad")
+    localStorage.removeItem("ttcs-testLoad")
+    expect(test).toEqual({test:true})
+  })
+})
