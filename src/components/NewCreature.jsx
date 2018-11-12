0ccb483f3f25848e0bfc250a7ad6ab4b279f12e7
Aaron <aaron4uk@gmail.com>
2018-11-11 19:56:40 -0600

    initial commit

---
+++ src/components/NewCreature.jsx
@@ -0,0 +1,18 @@
+import {connect} from "react-redux";
+import {Button} from "@material-ui/core/";
+
+import CreateCreature from "./../store/dispatcher/CreateCreature";
+
+const makeNewCreature = (dispatch) => {
+  CreateCreature(dispatch);
+  
+};
+
+const NewCreature = (props) => {
+  const {dispatch} = props;
+  return <Button variant="contained" color="primary" onClick={()=>{makeNewCreature(dispatch);}}>
+    New Creature
+  </Button>;
+};
+
+export default connect()(NewCreature);
