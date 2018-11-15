1969bbbd092396df401cb07f51eda586cee0c99a
Aaron <aaron4uk@gmail.com>
2018-11-15 05:56:34 -0600

    moved folder

--- src/__test__/components/NewCreature.test.jsx
+++ src/__test__/components/NewCreature.test.jsx
@@ -2,13 +2,11 @@ import { connect } from "react-redux"
 import { mountWithStore } from "enzyme-redux"
 import { createMockStore } from "redux-test-utils"
 import toJson from "enzyme-to-json"
-import sinon from "sinon"
 
-import NewCreature from "./NewCreature"
-import CreateCreature from "./../store/action/CreateCreature"
-import mockStoreState from "./../testHelpers/mockStoreState"
+import NewCreature from "../../components/NewCreature"
+import mockStoreState from "../../testHelpers/mockStoreState"
 
-describe("<New Creature />", ()=>{
+describe("<NewCreature />", ()=>{
   const ReactComponent = () => <NewCreature />
   const mapStateToProps = (state) => ({state})
   const store = createMockStore(mockStoreState)
@@ -19,14 +17,6 @@ describe("<New Creature />", ()=>{
     expect(typeof ReactComponent).toBe("function")
     expect(typeof component).toBe("object")
   })
-
-  // it("calls a CreateCreature", ()=>{
-  //   var spy = sinon.spy(CreateCreature)
-  //   const button = component.find("Button")
-  //   expect(button.length).toBe(1)
-  //   button.simulate("click")
-  //   expect(spy.calledOnce).toBe(true)
-  // })
 
   it("Snapshots", () => {
     expect(toJson(component)).toMatchSnapshot()
