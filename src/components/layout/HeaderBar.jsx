1803d7e9807dc06e09c92e9b497462c64cce1d33
Aaron <aaron4uk@gmail.com>
2018-11-16 05:26:10 -0600

    semi cleanse, code cleanup

--- src/components/layout/HeaderBar.jsx
+++ src/components/layout/HeaderBar.jsx
@@ -1,27 +1,26 @@
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { connect } from "react-redux";
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import { connect } from "react-redux"
 
-import ToggleSidebar from "./../../store/action/ToggleSidebar";
+import ToggleSidebar from "./../../store/action/ToggleSidebar"
 
 const HeaderBar = (props) => {
-  const {dispatch} = props;
+  const {dispatch} = props
   return <AppBar>
     <Toolbar>
-      <IconButton color="inherit" aria-label="Menu" onClick={()=>{ return ToggleSidebar(dispatch); }} >
+      <IconButton color="inherit" aria-label="Menu" onClick={()=>{ return ToggleSidebar(dispatch) }} >
         <MenuIcon />
       </IconButton>
       <Typography variant="h4" color="inherit">Tabletop Combat Simulator</Typography>
     </Toolbar>
-  </AppBar>;
-};
+  </AppBar>
+}
 
 const mapStateToProps = (state) => {
-  const open = state.sideBar;
-  return {open};
-};
+  return {open:state.sideBar}
+}
 
-export default connect(mapStateToProps)(HeaderBar);
+export default connect(mapStateToProps)(HeaderBar)
