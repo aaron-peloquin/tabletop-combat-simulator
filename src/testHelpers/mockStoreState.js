fbaa90ff15330c538919235b365da58ea99108f9
Aaron <aaron4uk@gmail.com>
2018-11-16 05:36:43 -0600

    adding mock data

--- src/testHelpers/mockStoreState.js
+++ src/testHelpers/mockStoreState.js
@@ -1,4 +1,45 @@
 export default {
-  creatures: [],
-  sideBar: false,
-};
+  "creatures": [
+    {
+      "name": "Goplin",
+      "hash": "hivn3p0k",
+      "description": "A stout monster",
+      "defaultInitiative": 10,
+      "armor": 11,
+      "hp": 5,
+      "hitDiceEquation": "1d20+1",
+      "damageDiceEquation": "1d4"
+    },
+    {
+      "name": "Elven Ranger",
+      "hash": "h3dxf5wl",
+      "description": "A swift hunter",
+      "defaultInitiative": 16,
+      "armor": 13,
+      "hp": 10,
+      "hitDiceEquation": "1d20+8",
+      "damageDiceEquation": "1d6+3"
+    },
+    {
+      "name": "Knight",
+      "hash": "hee9p286",
+      "description": "An honorable soldier",
+      "defaultInitiative": 11,
+      "armor": 18,
+      "hp": 25,
+      "hitDiceEquation": "1d20+4",
+      "damageDiceEquation": "1d12+4"
+    },
+    {
+      "name": "Lizardfolk",
+      "hash": "howx26rt",
+      "description": "An ancient sneaky monster",
+      "defaultInitiative": 14,
+      "armor": 14,
+      "hp": 15,
+      "hitDiceEquation": "1d20+5",
+      "damageDiceEquation": "1d10+2"
+    }
+  ],
+  "sideBar": false,
+}
