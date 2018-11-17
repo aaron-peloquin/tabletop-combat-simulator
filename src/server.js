0ac6fe565f0ef38daddbf64569b08b7f4d1cca17
Aaron <aaron4uk@gmail.com>
2018-11-16 20:23:05 -0600

    semi cleanse

--- src/server.js
+++ src/server.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const next = require("next");
+const express = require("express")
+const next = require("next")
 
 const server = (dev=true) => {
-  const port = (dev?3000:80);
-  const dir = "./src/";
-  const app = next({ dev, dir });
-  const handle = app.getRequestHandler();
+  const port = (dev?3000:80)
+  const dir = "./src/"
+  const app = next({ dev, dir })
+  const handle = app.getRequestHandler()
   app.prepare().then(() => {
-    const server = express();
+    const server = express()
 
     server.get("/creature/:hash", (req, res) => {
-      return app.render(req, res, "/creature", { hash: req.params.hash });
-    });
+      return app.render(req, res, "/creature", { hash: req.params.hash })
+    })
 
     server.get("*", (req, res) => {
-      return handle(req, res);
-    });
+      return handle(req, res)
+    })
 
     server.listen(port, (err) => {
       if (err) {
-        throw err;
+        throw err
       }
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-  });
-};
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
+}
 
-module.exports = server;
+module.exports = server
