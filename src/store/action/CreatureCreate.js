f9ce6b59394467e27e61509b16fbc6ead5362094
Aaron <aaron4uk@gmail.com>
2018-11-19 05:13:12 -0600

    adding comment

--- src/store/action/CreatureCreate.js
+++ src/store/action/CreatureCreate.js
@@ -1,5 +1,10 @@
 import actionTypes from "../actionTypes"
 
+/**
+ * Disapatches an action to make a new creature
+ * @param {func} dispatch the redux dispatcher action
+ * @param {obj} payload of creature's starting data
+ */
 const CreatureCreate = (dispatch, payload) => {
   const data = {
     type: actionTypes.CreatureCreate,
