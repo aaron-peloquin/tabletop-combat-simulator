5582b3109f3d63a989ac661a076949512255380c
Aaron <aaron4uk@gmail.com>
2018-11-20 22:08:42 -0600

    adding grid formatting and a link to /new-creature

--- src/pages/creatures.jsx
+++ src/pages/creatures.jsx
@@ -1,17 +1,25 @@
 import {Fragment} from "react"
-import CreatureFormNew from "../components/CreatureFormNew"
-import CreaturesList from "./../components/CreaturesList"
-import { connect } from "react-redux"
-import { Button } from "@material-ui/core"
+import {connect} from "react-redux"
+import { withStyles } from "@material-ui/core/styles"
+import {Button, Grid} from "@material-ui/core"
+import Link from "next/link"
 
 import CreatureDeleteAll from "./../store/action/CreatureDeleteAll"
+import CreaturesList from "./../components/CreaturesList"
 
 const creatures = (props) => {
-  const {dispatch, router} = props
+  const {dispatch, classes} = props
 
   return <Fragment>
-    <CreatureFormNew router={router} />
-    <Button onClick={()=>{CreatureDeleteAll(dispatch,"delete all")}} color="secondary">DELETE ALL</Button>
+    <Grid container justify="center">
+      <Grid item xs={6}>
+        <Link href="/new-creature"><a className={classes.link}><Button variant="contained" color="primary">New Creature</Button></a></Link>
+      </Grid>
+      <Grid item xs={6}>
+        <Button onClick={()=>{CreatureDeleteAll(dispatch,"delete all")}} variant="contained" color="secondary">Delete All Creatures</Button>
+      </Grid>
+    </Grid>
+    
     <CreaturesList />
   </Fragment>
 }
@@ -21,4 +29,11 @@ creatures.getInitialProps = async () => {
   return { title }
 }
 
-export default connect()(creatures)
+const styles = {
+  link: { textDecoration: "none", },
+}
+
+/** Add MUI styles to this component */
+const creaturesWithStyles = withStyles(styles)(creatures)
+
+export default connect()(creaturesWithStyles)
