bf3ab782ed44527b9709f189a62f71a3f14b7c45
Aaron <aaron4uk@gmail.com>
2018-11-27 05:53:12 -0600

    code cleanup

--- src/components/CreatureFormNew.jsx
+++ src/components/CreatureFormNew.jsx
@@ -3,11 +3,9 @@ import {connect} from "react-redux"
 import CreatureForm from "./CreatureForm"
 import CreatureCreate from "./../store/action/CreatureCreate"
 
-let data = {}
-
 const CreatureFormNew = (props) => {
   const {dispatch, router} = props
-  return <CreatureForm router={router} creature={data} onSubmit={(payload)=>{CreatureCreate(dispatch, payload)}} />
+  return <CreatureForm router={router} creature={{}} onSubmit={(payload)=>{CreatureCreate(dispatch, payload)}} />
 }
 
 export default connect()(CreatureFormNew)
