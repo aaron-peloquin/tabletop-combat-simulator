432946f834a3b2e435c38db864c1bd4625869021
Aaron <aaron4uk@gmail.com>
2018-11-28 16:03:23 -0600

    initial commit

---
+++ src/store/reducer/sideBar.test.js
@@ -0,0 +1,24 @@
+import actionTypes from "./../actionTypes"
+
+import sideBar from "./sideBar"
+
+describe("[reduxReducer] sideBar", ()=>{
+  let result, state, payload
+  beforeEach(()=>{
+    payload = { type: actionTypes.ToggleSidebar }
+    result = null
+    state = null
+  })
+
+  it("[ToggleSidebar] toggles state true when given false", ()=>{
+    state = false
+    result = sideBar(state, payload)
+    expect(result).toBe(true)
+  })
+
+  it("[ToggleSidebar] toggles state false when given true", ()=>{
+    state = true
+    result = sideBar(state, payload)
+    expect(result).toBe(false)
+  })
+})
