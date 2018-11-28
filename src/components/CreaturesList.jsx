78070ba8ac5480a381ae85865a7aceb699aa06c4
Aaron <aaron4uk@gmail.com>
2018-11-28 05:51:43 -0600

    abstracting into two components

--- src/components/CreaturesList.jsx
+++ src/components/CreaturesList.jsx
@@ -1,62 +1,39 @@
-import { connect } from "react-redux"
-import Link from "next/link"
 import {
-  Button,
   Table,
   TableHead, TableBody,
   TableRow, TableCell,
-  TableSortLabel,
 } from "@material-ui/core/"
-import {FilterList, Edit, Delete} from "@material-ui/icons"
+import { connect } from "react-redux"
 
-import CreatureDeleteOne from "./../store/action/CreatureDeleteOne"
-
-const CreatureRow = ({data, dispatch}) => {
-  return <TableRow key={data.hash}>
-    <TableCell>
-      <Link as={`/creature/${data.hash}`} href={`/creature?hash=${data.hash}`}>
-        <a>
-          <Button variant="contained" color="primary">
-            <Edit />
-          </Button>
-        </a>
-      </Link>
-      <Button variant="contained" color="secondary" onClick={()=>{CreatureDeleteOne(dispatch, data.hash)}}>
-        <Delete />
-      </Button>
-    </TableCell>
-    <TableCell>{data.name}</TableCell>
-    <TableCell>{data.cr}</TableCell>
-  </TableRow>
-}
-
-const CreaturesTable = ({data, dispatch}) => {
-  if(data.length>0) {
-    const columns = ["","Name","CR"]
-    return <Table>
-      <TableHead>
-        <TableRow>
-          {columns.map((label)=>{return <TableCell key={label}>{label}</TableCell>})}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((d)=><CreatureRow key={d.hash} data={d} dispatch={dispatch} />)}
-      </TableBody>
-    </Table>
-  }
-  return <p data-class="no-creatures">No creatures exist yet</p>
-}
+import CreaturesTableRow from "./CreaturesTableRow"
 
 const CreaturesList = (props) => {
-  const {creatures, numCreatures, dispatch} = props
-  return <div>
-    <p>Showing {numCreatures} Creatures</p>
-    <CreaturesTable data={creatures} dispatch={dispatch} />
-  </div>
+  const { creatures, numCreatures } = props
+  const columns = [ "", "Name", "CR" ]
+  let html
+  if(creatures.length===0) {
+    html = <p>No creatures exist yet</p>
+  }
+  else {
+    html = <div>
+      <p>Showing {numCreatures} Creatures</p>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((label)=>{return <TableCell key={`heading-${label}`}>{label}</TableCell>})}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {creatures.map((c)=><CreaturesTableRow key={`creature-${c.hash}`} creature={c} />)}
+        </TableBody>
+      </Table>
+    </div>
+  }
+  return html
 }
 
-const mapStateToProps = (state) => {
-  const {creatures=[]} = state
+export const mapStateToProps = (state) => {
+  const { creatures=[] } = state
   const sortedCreatures = creatures.sort((a,b)=>{
     let r = 0
     if(a.name<b.name) { r = -1 }
