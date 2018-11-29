295894d1bdbbe19376b57baa742f4142476807ef
Aaron <aaron4uk@gmail.com>
2018-11-29 17:40:35 -0600

    semi cleanse

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,23 +1,22 @@
-import {Grid,Paper} from "@material-ui/core";
-import {withStyles} from "@material-ui/core/styles";
+import {Grid,Paper} from "@material-ui/core"
+import {withStyles} from "@material-ui/core/styles"
 
-import HeaderBar from "./HeaderBar";
-import SideBar from "./SideBar";
+import HeaderBar from "./HeaderBar"
+import SideBar from "./SideBar"
 
 const styles = (theme) => {
   return {
     content: {
       margin: "0 auto",
       marginTop: "90px",
-      //maxWidth: "1000px",
       width: "80%",
     },
     paper: {
       minHeight: "500px",
       padding: theme.spacing.unit * 4,
     }
-  };
-};
+  }
+}
 
 const Layout = ({children="No page content was passed",classes}) => {
   return (
@@ -34,6 +33,6 @@ const Layout = ({children="No page content was passed",classes}) => {
         </Grid>
       </main>
     </div>
-  );};
+  )}
 
-export default withStyles(styles)(Layout);
+export default withStyles(styles)(Layout)
