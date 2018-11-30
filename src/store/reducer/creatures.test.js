44583dcffa9102ac4b43751822dcd77ef4f2815c
Aaron <aaron4uk@gmail.com>
2018-11-30 01:14:36 -0600

    initial commit

---
+++ src/store/reducer/creatures.test.js
@@ -0,0 +1,48 @@
+import actionTypes from "./../actionTypes"
+
+import creatures from "./creatures"
+
+describe("[reduxReducer] creatures", ()=>{
+  let state, data
+  beforeEach(()=>{
+    data = {}
+    data.type = actionTypes.CreatureCreate
+    data.payload = { name: "Hello World" }
+    state = creatures(state, data)
+    data = {}
+  })
+
+  it("[CreatureCreate] adds the new creature in as 0", ()=>{
+    expect(state[0].name).toBe("Hello World")
+  })
+
+  it("[CreatureDeleteAll] deletes all creature data", ()=>{
+    expect(state.length).toBe(2)
+    data = { type: actionTypes.CreatureDeleteAll }
+    state = creatures(state, data)
+    expect(state.length).toBe(0)
+  })
+
+  it("[CreatureDeleteOne] modifies creature data", ()=>{
+    expect(state.length).toBe(1)
+    data = {
+      type: actionTypes.CreatureDeleteOne,
+      payload: state[0].hash
+    }
+    state = creatures(state, data)
+    expect(state.length).toBe(0)
+  })
+
+  it("[CreatureUpdate] modifies creature data", ()=>{
+    console.log("statestate", state)
+    data = {
+      type: actionTypes.CreatureUpdate,
+      payload: {
+        name: "New Name",
+        hash: state[0].hash
+      }
+    }
+    state = creatures(state, data)
+    expect(state[0].name).toBe("New Name")
+  })
+})
