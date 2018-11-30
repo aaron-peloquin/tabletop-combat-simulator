c991239c6ad171acd5a790b0197de4f47e6f8c19
Aaron <aaron4uk@gmail.com>
2018-11-29 21:36:01 -0600

    fixing shallow renderer

--- src/pages__test__/about.test.jsx
+++ src/pages__test__/about.test.jsx
@@ -4,7 +4,7 @@ import toJson from "enzyme-to-json"
 import About from "../pages/about"
 
 describe("<Creature /> Page", ()=>{
-  const RenderShallowUntilComponent = createShallow({"untilSelector":"About"})
+  const RenderShallowUntilComponent = createShallow()
   let props, Component
 
   beforeEach( async ()=>{
