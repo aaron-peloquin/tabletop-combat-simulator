453f6ba1767eb3e4d614edb4c54b9f285acd25fc
Aaron <aaron4uk@gmail.com>
2018-12-04 15:48:00 -0600

    removing old comments

--- src/pages__test__/about.test.jsx
+++ src/pages__test__/about.test.jsx
@@ -24,32 +24,3 @@ describe("<Creature /> Page", ()=>{
     expect(toJson(Component)).toMatchSnapshot()
   })
 })
-
-// import { connect } from "react-redux"
-// import { shallowWithStore } from "enzyme-redux"
-// import { createMockStore } from "redux-test-utils"
-// import mockStoreState from "../testHelpers/mockStoreState"
-// import toJson from "enzyme-to-json"
-
-// import About from "../pages/about"
-
-// describe("<About />", ()=>{
-//   const ReactComponent = () => <About />
-//   const mapStateToProps = (state) => ({state})
-//   const store = createMockStore(mockStoreState)
-//   const ConnectedComponent = connect(mapStateToProps)(ReactComponent)
-//   const component = shallowWithStore(<ConnectedComponent />, store)
-
-//   it("loads", async () => {
-//     expect(typeof component).toBe("object")
-//   })
-
-//   it("has a NextJS prop.title of About", async () => {
-//     const props = await About.getInitialProps()
-//     expect(props.title).toBe("About")
-//   })
-
-//   it("Snapshots", () => {
-//     expect(toJson(component)).toMatchSnapshot()
-//   })
-// })
