7f68480b09f993d4d9f76e258f1d208d99856192
Aaron <aaron4uk@gmail.com>
2018-12-08 14:08:14 -0600

    reworking new ui onto homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,22 @@
-import React, { Fragment } from "react"
+import React from "react"
+import { Grid } from "@material-ui/core"
+
+import CreatureForm from "./../components/CreatureForm"
 
 const Index = () => {
-  return <Fragment>
-    Hello, homepage.
-  </Fragment>
+  return <Grid container spacing={24}>
+    <Grid item xs={12}>
+      <CreatureForm />
+    </Grid>
+    <Grid item xs={12} container spacing={24}>
+      <Grid item sm={6}>Team A</Grid>
+      <Grid item sm={6}>Team B</Grid>
+    </Grid>
+    <Grid item xs={12} container spacing={24}>
+      <Grid item sm={6}>Simulation Results</Grid>
+      <Grid item sm={6}>Simulation Log</Grid>
+    </Grid>
+  </Grid>
 }
 
 Index.getInitialProps = () => {
