a5f84a137d6a713a256a99874c50bd30786cca6a
Aaron <aaron4uk@gmail.com>
2018-12-17 05:33:51 -0600

    removed test - no longer applies to expected functionality

--- src/components/CreatureForm.jsx
+++ src/components/CreatureForm.jsx
@@ -1,4 +1,5 @@
-import {Button,Grid} from "@material-ui/core/"
+import { Button, Grid } from "@material-ui/core/"
+import { withStyles } from "@material-ui/core/styles"
 import { connect } from "react-redux"
 
 import CreatureFormGridField from "./CreatureFormGridField"
@@ -14,7 +15,8 @@ import standardizeCreatureData from "./../helpers/standardizeCreatureData"
  *    @param {func} onSubmit called when the form is submitted
  */
 const CreatureForm = (props) => {
-  const {creature={}, creatures={}, router, onChange, onSubmit} = props
+  const { creature={}, creatures={}, onChange, onSubmit, router, classes } = props
+  let buttonText = (typeof creature.hash === "undefined"?"Create":"Save")
   let _data = standardizeCreatureData(creature, creatures)
   
   const FieldProps = {
@@ -25,13 +27,23 @@ const CreatureForm = (props) => {
   /** Handles form submissions and the onSubmit callback */
   const submitData = (e) => {
     e.preventDefault()
+
+    if(_data.team.length === 0) {
+      _data.team = "a"
+    }
+
+    console.log("_data.team", typeof _data.team, _data.team)
+
     if(_data.name.length>0 && typeof onSubmit=="function") {
       onSubmit(_data)
     }
-    if(typeof router === "object") {
-      /** does not seem to work yet */
-      router.push("/creatures")
-    }
+    /** Clear the form */
+    _data = standardizeCreatureData({}, creatures)
+  }
+
+  const submitDataTeamB = (e) => {
+    _data.team = "b"
+    submitData(e)
   }
 
   return <form autoComplete="off" onSubmit={submitData}>
@@ -47,17 +59,33 @@ const CreatureForm = (props) => {
 
         <CreatureFormGridField {...FieldProps} size="medium" dataKey="hitDiceEquation" label="Hit Dice Equation" placeholder="eg. 1d20+4" />
         <CreatureFormGridField {...FieldProps} size="medium" dataKey="damageDiceEquation" label="Damage Dice Equation" placeholder="eg. 1d6+2d4+2" />
-
-        <CreatureFormGridField {...FieldProps} size="large" field="multiline" dataKey="description" label="Description" />
       </Grid>
-      <Button type="submit" variant="contained" color="primary" onClick={submitData}>Save</Button>
+      <Grid container justify="center">
+        <Grid item sm={6}>
+          <Button className={classes.submitButtons} type="submit" variant="contained" color="primary" onClick={submitData}>{buttonText} to Team A</Button>
+        </Grid>
+        <Grid item sm={6}>
+          <Button className={classes.submitButtons} type="submit" variant="contained" color="secondary" onClick={submitDataTeamB}>{buttonText} to Team B</Button>
+        </Grid>
+      </Grid>
     </fieldset>
   </form>
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, props) => {
   return {creatures:state.creatures}
 }
 
-const CreatureFormConnected = connect(mapStateToProps)(CreatureForm)
-export default CreatureFormConnected
+const styles = (theme) => {
+  return {
+    submitButtons: {
+      width: "90%",
+      //justify: "center",
+      marginTop: theme.spacing.unit
+    }
+  }
+}
+
+const CreatureFormStyled = withStyles(styles)(CreatureForm)
+const CreatureFormStyledConnected = connect(mapStateToProps)(CreatureFormStyled)
+export default CreatureFormStyledConnected
--- src/components/CreatureForm.test.jsx
+++ src/components/CreatureForm.test.jsx
@@ -13,7 +13,7 @@ describe("<CreatureForm />", ()=>{
     store = initializeStore(mockStoreState)
     props = {
       onSubmit: jest.fn(()=>{}),
-      router: {push:jest.fn(() => {})},
+      router: { push: jest.fn(() => {}) },
       creature: mockStoreState.creatures[0]
     }
     Component = RenderShallowUntilComponent(<Provider store={store}><CreatureForm {...props} /></Provider>)
@@ -30,12 +30,6 @@ describe("<CreatureForm />", ()=>{
   it("fires submit callback", ()=>{
     Component.simulate("submit", {preventDefault:()=>{}})
     expect(props.onSubmit).toHaveBeenCalledTimes(1)
-  })
-
-  it("submit routes to /creatures", () => {
-    Component.simulate("submit", {preventDefault:()=>{}})
-    expect(props.router.push).toHaveBeenCalledTimes(1)
-    expect(props.router.push).toHaveBeenCalledWith("/creatures")
   })
 
   it("snapshots", () => {
