741c3e630123aef8b75e31626b8de53060adb8e6
Aaron <aaron4uk@gmail.com>
2018-12-17 05:36:46 -0600

    adding onSubmit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,20 @@
 import React from "react"
 import { Grid } from "@material-ui/core"
+import { connect } from "react-redux"
 
 import CreatureForm from "./../components/CreatureForm"
+import CreatureCreateUpdate from "./../store/action/CreatureCreateUpdate"
 
-const Index = () => {
+const Index = (props) => {
+  const { dispatch, FuncCreatureCreateUpdate } = props
+
+  const runUpdate = (creatureData) => {
+    FuncCreatureCreateUpdate(dispatch, creatureData)
+  }
+
   return <Grid container spacing={24}>
     <Grid item xs={12}>
-      <CreatureForm />
+      <CreatureForm onSubmit={runUpdate} />
     </Grid>
     <Grid item xs={12} container spacing={24}>
       <Grid item sm={6}>Team A</Grid>
@@ -19,8 +27,13 @@ const Index = () => {
   </Grid>
 }
 
+Index.defaultProps = {
+  FuncCreatureCreateUpdate: CreatureCreateUpdate,
+}
+
 Index.getInitialProps = () => {
   const title = "Tabletop Combat Simulator"
   return { title }
 }
-export default Index
+
+export default connect()(Index)
