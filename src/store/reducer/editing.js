0e8ec0198f023b4f75f2b498e34107314a1670ac
Aaron <aaron4uk@gmail.com>
2018-12-18 14:46:13 -0600

    initial commit

---
+++ src/store/reducer/editing.js
@@ -0,0 +1,39 @@
+import actionTypes from "./../actionTypes"
+
+const defaultState = {}
+
+/**
+ * Reducer for the redux key of "editing".
+ * This is the creature data currently loaded into our form for a visitor to edit
+ * See: README.md for details on this data structure
+ *
+ * @param {obj} state the array of creature objects
+ * @param {obj} data
+ *   @param {str} type a String to identify how we want to update the state
+ *   @param {obj} payload the data we use to update the state
+ * @returns {obj} The new state of creature
+ */
+const editingReducer = (state=defaultState, { type=false, payload={} }) => {
+
+  switch (type) {
+  default:
+    /** do nothing */
+    break
+
+  /** Payload: { starting creature data } */
+  case actionTypes.SaveCreature:
+    /** Clear the state */
+    state = state.slice()
+    state = defaultState
+    break
+
+  case actionTypes.SetEditCreature:
+    /** Set a new creature into the state */
+    state = state.slice()
+    state = payload
+    break
+  }
+  return state
+}
+
+export default editingReducer
