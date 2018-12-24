60ff0565b67c62a881cbd563c71420aeee221943
Aaron <aaron4uk@gmail.com>
2018-12-24 06:44:38 -0600

    added test

--- src/store/reducer/editing.test.js
+++ src/store/reducer/editing.test.js
@@ -5,7 +5,7 @@ import editing from "./editing"
 describe("[reduxReducer] editing", ()=>{
   let result, state, data
   beforeEach(()=>{
-    state = { name: "Hello World" }
+    state = { name: "Hello World", hash: "ht3st" }
     data = { type: "", payload: state }
     result = null
   })
@@ -21,5 +21,13 @@ describe("[reduxReducer] editing", ()=>{
     data.payload = { name: "World, hello" }
     result = editing(state, data)
     expect(result).toEqual(data.payload)
+  })
+
+  it("[UpdateEditCreature] updates a key", () => {
+    data.type = actionTypes.UpdateEditCreature
+    data.payload = { key:"name", value:"New Name Value" }
+    result = editing(state, data)
+    console.log("result.name", result.name)
+    expect(result.name).toBe("New Name Value")
   })
 })
