288718ffa0763c3cc5bbfd47155e5f23f5b12628
Aaron <aaron4uk@gmail.com>
2018-12-26 05:41:33 -0600

    updating component to use MapActionsToProps

--- src/components/DeleteAllCreatures.jsx
+++ src/components/DeleteAllCreatures.jsx
@@ -4,14 +4,12 @@ import {Button} from "@material-ui/core"
 import DeleteAll from "./../store/action/DeleteAllCreatures"
 
 const DeleteAllCreatures = (props) => {
-  const { dispatch, DeleteAll } = props
-  return <Button data-id="delete-all" onClick={()=>{
-    DeleteAll(dispatch,"delete all")
-  }} variant="contained" color="secondary">Delete All Creatures</Button>
+  const { ActionDeleteAll } = props
+  return <Button data-id="delete-all" onClick={ActionDeleteAll} variant="contained" color="secondary">Delete All Creatures</Button>
 }
 
-DeleteAllCreatures.getInitialProps = async () => {
-  return { ActionDeleteAll: DeleteAll }
+const MapActionsToProps = (dispatch) => {
+  ActionDeleteAll = () => { DeleteAll(dispatch,"delete all") },
 }
 
-export default connect()(DeleteAllCreatures)
+export default connect({}, MapActionsToProps)(DeleteAllCreatures)
