36943d370f5e9b0b1e690592030156d0dda75c6e
Aaron <aaron4uk@gmail.com>
2018-12-29 09:15:12 -0600

    added data cleansing

--- src/store/action/UpdateEditCreature.js
+++ src/store/action/UpdateEditCreature.js
@@ -1,6 +1,19 @@
 import actionTypes from "./../actionTypes"
 
 const UpdateEditCreature = (dispatch, key, value) => {
+  /** Cleanse data */
+  switch (key) {
+  case "hp":
+  case "armor":
+  case "initiative":
+    value = value.replace(/\D/g, "")
+    break
+  case "hitDiceEquation":
+  case "damageDiceEquation":
+    value = value.replace(/([^\d|d|\+|\-|\*|\/|\(|\)])/gi, "").toLowerCase()
+    break
+  }
+
   const data = {
     type: actionTypes.UpdateEditCreature,
     payload: {key, value},
