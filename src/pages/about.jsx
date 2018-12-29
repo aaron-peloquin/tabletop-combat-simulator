6f6812f6b550c5fe0b54fc7f2c7057b1fc5364ed
Aaron <aaron4uk@gmail.com>
2018-12-29 08:11:04 -0600

    adding copy to the about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,32 @@
-import React, { Fragment } from "react"
+import React, {Fragment} from "react"
+import {
+  Typography,
+} from "@material-ui/core"
 
 const About = () => {
   return <Fragment>
-    <p>Some about page copy.</p>
+    <Typography variant="h2">About</Typography>
+    <Typography variant="body2">
+      The goal of this website is to help simulate simple combat scenarios for your tabletop game.
+    </Typography>
+    <Typography variant="h4">App Code</Typography>
+    <Typography variant="body2">
+      The code for this web app is open-source,
+      see <a href="https://github.com/aaron-peloquin/tabletop-combat-simulator">Github</a>.
+    </Typography>
+    <Typography variant="h4">Thanks</Typography>
+    <Typography variant="body2">
+      Special thanks to <a target="_blank" href="https://game-icons.net/">Game-Icons.net</a>,
+      who provided the icons on this app under under
+      <a target="_blank" href="https://creativecommons.org/licenses/by/3.0/">Creative Commons</a>.
+      see <a href="https://github.com/aaron-peloquin/tabletop-combat-simulator">Github</a>.
+    </Typography>
   </Fragment>
 }
 
 About.getInitialProps = async () => {
-  let title = "About"
-  return { title }
+  const title = "About"
+  return {title}
 }
 
 export default About
