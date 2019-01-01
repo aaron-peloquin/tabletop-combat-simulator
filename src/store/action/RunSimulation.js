16bf6fcad5ae3fc76cacce1dd57733add0ba7628
Aaron <aaron4uk@gmail.com>
2019-01-01 01:29:31 -0600

    initial commit

---
+++ src/store/action/RunSimulation.js
@@ -0,0 +1,17 @@
+import actionTypes from "../actionTypes"
+
+/**
+ * Disapatches an action to run a combat simulation
+ * @param {func} dispatch the redux dispatcher action
+ * @param {obj} payload should be all creatures on all teams
+ * @return {void}
+ */
+const RunSimulation = (dispatch, payload) => {
+  const data = {
+    type: actionTypes.RunSimulation,
+    payload,
+  }
+  dispatch(data)
+}
+
+export default RunSimulation
---
+++ src/store/action/RunSimulation.test.js
@@ -0,0 +1,12 @@
+import runSimulation from "./RunSimulation"
+import mockStoreState from "./../../testHelpers/mockStoreState"
+
+describe("[reduxAction] RunSimulation", ()=>{
+  const mockFunction = jest.fn(()=>{})
+  const mockValidPayload = mockStoreState
+
+  it("mockFunction is called with valid payload", ()=>{
+    runSimulation(mockFunction, mockValidPayload)
+    expect(mockFunction).toBeCalledTimes(1)
+  })
+})
