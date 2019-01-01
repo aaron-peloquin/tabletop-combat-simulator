4efc9d73c7ae262cf801479918e0b0d15848df0c
Aaron <aaron4uk@gmail.com>
2019-01-01 02:03:11 -0600

    started writing code, testing rolling dice

--- src/store/reducer/combat.js
+++ src/store/reducer/combat.js
@@ -1,14 +1,20 @@
+import roll from "./../../helpers/diceRolling/"
 import actionTypes from "./../actionTypes"
 
+const defaultState = {
+  AliveTeamCreatures: {a:[], b:[]},
+  TurnOrder: [],
+  CreatureStatus: [],
+}
 /**
  * @param {obj} state The current state of `combat`
  * @param {obj} data
  *  @param {str} type The redux action type
  *  @param {obj} payload The data used to update the stae, based upon the type
- * @return the new state
+ * @return {obj} the new state
  */
-
 const combatReducer = (state=defaultState, {type=false, payload={}}) => {
+  console.log("type", typeof type, type)
   switch (type) {
   case actionTypes.RunSimulation:
     /**
@@ -23,10 +29,14 @@ const combatReducer = (state=defaultState, {type=false, payload={}}) => {
      *    D) if this damage reduces them to 0 or lower, remove the enemy from `state.AliveTeamCreatures`
      *    E) insert a row into `state.Log`, describing what happened
      */
+    console.log("payload", payload)
     if (payload.length>0) {
       payload.map((creature) => {
         if (creature.hp > 0) {
+          /** Sort concious creatures into teams A and B in `AliveTeamCreatures` */
           state.AliveTeamCreatures[creature.team] = creature.hash
+          /** Roll inititive, and add into `TurnOrder` */
+          console.log(roll("1D4+5"))
         }
       })
     }
