ac6229e35e850ed759cd871100ac37298bbcf97f
Aaron <aaron4uk@gmail.com>
2019-01-05 09:32:03 -0600

    adding tests

--- src/helpers/diceRolling/rollDie.test.js
+++ src/helpers/diceRolling/rollDie.test.js
@@ -14,6 +14,18 @@ describe("rollDie()", ()=>{
     expect(result).toBeGreaterThan(20)
   })
 
+  it("does not roll dice with more than 999 sides", () => {
+    equation = "1d99999999999999999999999999"
+    result = rollDie(equation).rollResult
+    expect(result).toBeLessThan(1000)
+  })
+
+  it("does not roll more than 999 dice at once", () => {
+    equation = "99999999999999999999999999d1"
+    result = rollDie(equation).rollResult
+    expect(result).toBe(999)
+  })
+
   it("returns plain numbers", () => {
     equation = "15g4"
     result = rollDie(equation).rollResult
