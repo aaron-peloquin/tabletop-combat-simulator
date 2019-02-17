b3ef8dc7db0e00d6c6c9cba871179b04ee48ace8
Aaron <aaron4uk@gmail.com>
2019-02-17 13:37:28 -0600

    improving test coverage

--- src/helpers/sortInitiative.test.js
+++ src/helpers/sortInitiative.test.js
@@ -5,6 +5,7 @@ const mockState = [
   ["b", 5, 5],
   ["c", 4, 6],
   ["d", 6, 5],
+  ["e", 6, 4],
 ]
 
 describe("sortInitiative()", ()=>{
@@ -16,8 +17,9 @@ describe("sortInitiative()", ()=>{
 
   it("sorts correctly based on key 1, then key 2 as the tie-breaker", () => {
     expect(result[0][0]).toBe("d")
-    expect(result[1][0]).toBe("b")
-    expect(result[2][0]).toBe("a")
-    expect(result[3][0]).toBe("c")
+    expect(result[1][0]).toBe("e")
+    expect(result[2][0]).toBe("b")
+    expect(result[3][0]).toBe("a")
+    expect(result[4][0]).toBe("c")
   })
 })
