579a0259cb5af0521ba60bcf2dfb80bc86d781b8
Aaron <aaron4uk@gmail.com>
2019-03-12 20:56:32 -0500

    minor test update

--- src/components/layout/index.test.jsx
+++ src/components/layout/index.test.jsx
@@ -3,6 +3,8 @@ import {createShallow} from "@material-ui/core/test-utils"
 import toJson from "enzyme-to-json"
 
 import Layout from "./index"
+import HeaderBar from "./HeaderBar"
+import SideBar from "./SideBar"
 
 describe("<Layout />", async ()=>{
   const renderShallowUntilComponent = createShallow({"untilSelector": "Layout"})
@@ -16,8 +18,8 @@ describe("<Layout />", async ()=>{
   })
 
   it("has a <HeaderBar />, <SideBar /> and <main />", () => {
-    expect(Component.find("Connect(HeaderBar)").length).toBe(1)
-    expect(Component.find("Connect(WithStyles(SideBar))").length).toBe(1)
+    expect(Component.find(HeaderBar).length).toBe(1)
+    expect(Component.find(SideBar).length).toBe(1)
     expect(Component.find("main").length).toBe(1)
   })
 
