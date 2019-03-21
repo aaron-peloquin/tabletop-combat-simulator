4fc1badaab51f75d6aa531a6d9222130bf35cfc7
Aaron <aaron4uk@gmail.com>
2019-03-20 19:41:35 -0500

    updating test

--- src/components/layout/HeaderBar.test.jsx
+++ src/components/layout/HeaderBar.test.jsx
@@ -6,32 +6,32 @@ import {initializeStore} from "../../store/store"
 import HeaderBar, {mapStateToProps} from "./HeaderBar"
 import mockStoreState from "./../../testHelpers/mockStoreState"
 
+import toggleSidebar from "./../../store/action/ToggleSidebar"
+import IconButton from "@material-ui/core/IconButton"
+
+jest.mock("./../../store/action/ToggleSidebar")
+
 describe("<HeaderBar />", ()=>{
   const renderShallowUntilComponent = createShallow({"untilSelector": "HeaderBar"})
-  let store; let props; let Component
-  beforeEach(async ()=>{
-    store = initializeStore(mockStoreState)
-    props = mapStateToProps(store.getState())
-    /** Attach spy to store.dispatch */
-    store.dispatch = jest.fn(store.dispatch)
-    Component = renderShallowUntilComponent(<Provider store={store}><HeaderBar {...props} /></Provider>)
-  })
+  const store = initializeStore(mockStoreState)
+  const props = mapStateToProps(store.getState())
+  const Component = renderShallowUntilComponent(<Provider store={store}><HeaderBar {...props} /></Provider>)
 
-  it("loads", async () => {
+  it("loads", () => {
     expect(typeof Component).toBe("object")
   })
 
   it("has an IconButton with prop of aria-label=Menu", () => {
-    expect(Component.find("WithStyles(IconButton)[aria-label=\"Menu\"]").length).toBe(1)
+    expect(Component.find(IconButton).length).toBe(1)
   })
+
   it("has a state of open=false by default", () => {
     expect(store.getState().sideBar).toBe(false)
   })
 
-  it("calls function, dispatch, and updates state on click of menu", () => {
-    Component.find("WithStyles(IconButton)[aria-label=\"Menu\"]").simulate("click")
-    expect(store.dispatch).toBeCalledTimes(1)
-    expect(store.getState().sideBar).toBe(true)
+  it("calls toggleSidebar", () => {
+    Component.find(IconButton).simulate("click")
+    expect(toggleSidebar).toBeCalledTimes(1)
   })
 
   it("snapshots", () => {
