dc3c12ea14eabe472e4fd392594ce84479cf0051
Aaron P <aaron4uk@gmail.com>
2019-04-27 23:00:21 -0500

    trying out cypress

---
+++ cypress/integration/cypress-happy.js
@@ -0,0 +1,11 @@
+
+
+describe("Integration test with visual testing", function() {
+  it("Loads the homepage", function() {
+    // Load the page or perform any other interactions with the app.
+    cy.visit("/")
+
+    // Take a snapshot for visual diffing
+    cy.percySnapshot()
+  })
+})
