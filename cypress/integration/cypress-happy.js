7ece98ca6b7aa1634f567dbafc5c7288f1263cbd
Aaron P <aaron4uk@gmail.com>
2019-05-05 12:28:33 -0500

    cypress assertions work, but cy.get() fails

--- cypress/integration/cypress-happy.js
+++ cypress/integration/cypress-happy.js
@@ -4,6 +4,8 @@ describe("Integration test with visual testing", function() {
   it("Loads the homepage", function() {
     // Load the page or perform any other interactions with the app.
     cy.visit("/")
+    const expectation = true
+    expect(expectation).to.equal(true)
 
     // Take a snapshot for visual diffing
     // cy.percySnapshot()
