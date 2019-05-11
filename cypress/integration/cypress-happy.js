2aa2ef23bb5be2eed0c586debc5fff2feed9a1da
Aaron P <aaron4uk@gmail.com>
2019-05-11 14:43:46 -0500

    seperated into it blocks that build on eachother

--- cypress/integration/cypress-happy.js
+++ cypress/integration/cypress-happy.js
@@ -3,17 +3,26 @@ describe('Integration test with visual testing', function() {
   it('Loads the homepage', function() {
     // Load the page or perform any other interactions with the app.
     cy.visit('http://localhost:3000')
-    cy.fillForm()
-    cy.fillForm({team: 'b'})
-
-
-    cy.get('[data-cy="SimulationResults"]').should('not.exist')
-    cy.get('[data-cy="SimulateCombat"]').click()
-    cy.get('[data-cy="SimulationResults"]').should('exist')
 
     // Take a snapshot for visual diffing
     // cy.percySnapshot()
 
     //
+  })
+
+  it('Adds a creature to team A', () => {
+    cy.fillForm({team: 'a'})
+    cy.get('[data-team="a"] [data-cy="TeamListCreature"]').should('have.length', 1)
+  })
+
+  it('Adds a creature to team B', () => {
+    cy.fillForm({team: 'b'})
+    cy.get('[data-team="b"] [data-cy="TeamListCreature"]').should('have.length', 1)
+  })
+
+  it('Simulates Combat and displays result', () => {
+    cy.get('[data-cy="SimulationResults"]').should('not.exist')
+    cy.get('[data-cy="SimulateCombat"]').click()
+    cy.get('[data-cy="SimulationResults"]').should('exist')
   })
 })
