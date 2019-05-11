3107cf2e937fa36eddf0e948557f417d6b2c4360
Aaron P <aaron4uk@gmail.com>
2019-05-11 13:49:32 -0500

    cleaned up data entry

--- cypress/support/commands/fillForm.js
+++ cypress/support/commands/fillForm.js
@@ -7,11 +7,11 @@ Cypress.Commands.add('fillForm', ({
   damageDiceEquation = '2d6 + 4',
   team = 'a',
 } = {}) => {
-  cy.get('[data-cy="name"]').type(creatureName)
-  cy.get('[data-cy="hp"]').type(hp)
-  cy.get('[data-cy="armor"]').type(armor)
-  cy.get('[data-cy="initiative"]').type(initiative)
-  cy.get('[data-cy="hitDiceEquation"]').type(hitDiceEquation)
-  cy.get('[data-cy="damageDiceEquation"]').type(damageDiceEquation)
+  cy.get('[data-cy="name"]').clear().type(creatureName)
+  cy.get('[data-cy="hp"]').clear().type(hp)
+  cy.get('[data-cy="armor"]').clear().type(armor)
+  cy.get('[data-cy="initiative"]').clear().type(initiative)
+  cy.get('[data-cy="hitDiceEquation"]').clear().type(hitDiceEquation)
+  cy.get('[data-cy="damageDiceEquation"]').clear().type(damageDiceEquation)
   cy.get(`Button[data-cy="${team}TeamSubmit"]`).click()
 })
