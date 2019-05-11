89710dab1832212c01e730cf711d3dcb7c97c8fe
Aaron P <aaron4uk@gmail.com>
2019-05-10 21:31:54 -0500

    got cypress working

--- cypress/integration/cypress-happy.js
+++ cypress/integration/cypress-happy.js
@@ -1,15 +1,15 @@
 
-
-describe("Integration test with visual testing", function() {
-  it("Loads the homepage", function() {
+describe('Integration test with visual testing', function() {
+  it('Loads the homepage', function() {
     // Load the page or perform any other interactions with the app.
-    cy.visit("/")
-    const expectation = true
-    expect(expectation).to.equal(true)
+    cy.visit('http://localhost:3000')
+    cy.get('input').first().type('Test Creature').next().type(50)
 
-    // Take a snapshot for visual diffing
-    // cy.percySnapshot()
 
-    cy.get("input").fist().type("Test Creature")
+    // expect(expectation).to.equal(true)
+    // // Take a snapshot for visual diffing
+    // // cy.percySnapshot()
+
+    //
   })
 })
---
+++ cypress/support/commands/fillForm.js
@@ -0,0 +1,3 @@
+Cypress.Commands.add('fillForm', () => {
+
+})
