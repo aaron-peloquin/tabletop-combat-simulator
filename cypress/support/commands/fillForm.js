90f8843805391bd8bf9b2bcd5dca2c3410d5c8bf
Aaron P <aaron4uk@gmail.com>
2019-05-10 22:52:52 -0500

    updated command

--- cypress/support/commands/fillForm.js
+++ cypress/support/commands/fillForm.js
@@ -5,12 +5,13 @@ Cypress.Commands.add('fillForm', ({
   initiative = 3,
   hitDiceEquation = '1d20 + 2',
   damageDiceEquation = '2d6 + 4',
-}) => {
-  cy.get('[dataKey="name"]').type(creatureName)
-  cy.get('[dataKey="hp"]').type(hp)
-  cy.get('[dataKey="armor"]').type(armor)
-  cy.get('[dataKey="initiative"]').type(initiative)
-  cy.get('[dataKey="hitDiceEquation"]').type(hitDiceEquation)
-  cy.get('[dataKey="damageDiceEquation"]').type(damageDiceEquation)
-  cy.get('Button[data-cy="submitForm"]').click()
+  team = 'a',
+} = {}) => {
+  cy.get('[data-cy="name"]').type(creatureName)
+  cy.get('[data-cy="hp"]').type(hp)
+  cy.get('[data-cy="armor"]').type(armor)
+  cy.get('[data-cy="initiative"]').type(initiative)
+  cy.get('[data-cy="hitDiceEquation"]').type(hitDiceEquation)
+  cy.get('[data-cy="damageDiceEquation"]').type(damageDiceEquation)
+  cy.get(`Button[data-cy="${team}TeamSubmit"]`).click()
 })
