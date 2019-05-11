7459adeefa175d8806b27fcf2be92bee9c44f4bd
Aaron P <aaron4uk@gmail.com>
2019-05-10 22:53:38 -0500

    added data-cy attribute

--- src/components/CreatureFormGridField.jsx
+++ src/components/CreatureFormGridField.jsx
@@ -1,9 +1,9 @@
 import {
   Grid,
   TextField,
-} from "@material-ui/core/"
-import {withStyles} from "@material-ui/core/styles"
-import {connect} from "react-redux"
+} from '@material-ui/core/'
+import {withStyles} from '@material-ui/core/styles'
+import {connect} from 'react-redux'
 
 /**
  * Builds <Grid> input fields for the <CreatureForm /> element
@@ -35,41 +35,44 @@ let CreatureFormGridField = (props) => {
    * @return {void}
    */
   const updateData = (e, key) => {
-    const curValue = e.target.value||""
+    const curValue = e.target.value||''
     dataValue = curValue
 
-    if (typeof onChange=="function") {
+    if (typeof onChange=='function') {
       onChange(key, curValue)
     }
   }
 
   const FieldProps = {
-    "onChange": (e)=>{
+    'onChange': (e)=>{
       updateData(e, dataKey)
     },
-    "className": classes.formField,
-    "value": dataValue,
-    "label": label,
+    'className': classes.formField,
+    'value': dataValue,
+    'label': label,
+    'inputProps': {
+      'data-cy': dataKey,
+    },
   }
 
   switch (field) {
   default:
     DataField = <TextField {...FieldProps} />
     break
-  case "multiline":
+  case 'multiline':
     DataField = <TextField multiline rows="3" {...FieldProps} />
     break
   }
 
   switch (size) {
   default:
-  case "small":
+  case 'small':
     wrappedField = <Grid item xs={12} sm={4} md={4}>{DataField}</Grid>
     break
-  case "medium":
+  case 'medium':
     wrappedField = <Grid item xs={12} sm={6}>{DataField}</Grid>
     break
-  case "large":
+  case 'large':
     wrappedField = <Grid item xs={12}>{DataField}</Grid>
     break
   }
@@ -80,7 +83,7 @@ let CreatureFormGridField = (props) => {
 const styles = (theme) => {
   return {
     formField: {
-      width: "100%",
+      width: '100%',
       marginTop: theme.spacing.unit,
       marginBottom: theme.spacing.unit,
       paddingLeft: theme.spacing.unit,
